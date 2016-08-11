49c32d0bf12a3b129510b7cc9d9fd193c841335e
Alex <alex@bimpson.com>
2016-08-11 16:08:32 +0100

    Demos fixed

--- demo/scripts.js
+++ demo/scripts.js
@@ -5,23 +5,23 @@ $(document).ready(function(){
 	
 	
 	/********** SLIDER 1 - FADE **********/
-	//$('#slider1 .slider').prrpleSlider();
+	$('#slider1 .slider').prrpleSlider();
 	
 	
 	/********** SLIDER 2 - FADE - LOOP **********/
-	/*$('#slider2 .slider').prrpleSlider({
+	$('#slider2 .slider').prrpleSlider({
 		loop:					true
 	});
 	
 	
 	/********** SLIDER 3 - SLIDE - HORIZONTAL **********/
-	/*$('#slider3 .slider').prrpleSlider({
+	$('#slider3 .slider').prrpleSlider({
 		transition: 			'slide'
 	});
 	
 	
 	/********** SLIDER 4 - SLIDE - HORIZONTAL - LOOP **********/
-	/*$('#slider4 .slider').prrpleSlider({
+	$('#slider4 .slider').prrpleSlider({
 		transition: 			'slide',
 		loop:					true,
 		loopSeamless:			false
@@ -29,7 +29,7 @@ $(document).ready(function(){
 	
 	
 	/********** SLIDER 5 - SLIDE - HORIZONTAL - LOOP - SEAMLESS **********/
-	/*$('#slider5 .slider').prrpleSlider({
+	$('#slider5 .slider').prrpleSlider({
 		transition: 			'slide',
 		loop:					true,
 		loopSeamless:			true
@@ -37,7 +37,7 @@ $(document).ready(function(){
 	
 	
 	/********** SLIDER 6 - SLIDE - HORIZONTAL - LOOP - SEAMLESS - RICH SWIPING **********/
-	/*$('#slider6 .slider').prrpleSlider({
+	$('#slider6 .slider').prrpleSlider({
 		transition: 			'slide',
 		loop:					true,
 		loopSeamless:			true,
@@ -46,7 +46,7 @@ $(document).ready(function(){
 	
 	
 	/********** SLIDER 7 - SLIDE - HORIZONTAL - LOOP - SEAMLESS - RICH SWIPING - CSS TRANSFORMS **********/
-	/*$('#slider7 .slider').prrpleSlider({
+	$('#slider7 .slider').prrpleSlider({
 		transition: 			'slide',
 		loop:					true,
 		loopSeamless:			true,
@@ -56,14 +56,14 @@ $(document).ready(function(){
 	
 	
 	/********** SLIDER 8 - SLIDE - VERTICAL **********/
-	/*$('#slider8 .slider').prrpleSlider({
+	$('#slider8 .slider').prrpleSlider({
 		direction:				'vertical',
 		transition: 			'slide'
 	});
 	
 	
 	/********** SLIDER 9 - SLIDE - VERTICAL - LOOP **********/
-	/*$('#slider9 .slider').prrpleSlider({
+	$('#slider9 .slider').prrpleSlider({
 		direction:				'vertical',
 		transition: 			'slide',
 		loop:					true,
@@ -72,7 +72,7 @@ $(document).ready(function(){
 	
 	
 	/********** SLIDER 10 - SLIDE - VERTICAL - LOOP - SEAMLESS **********/
-	/*$('#slider10 .slider').prrpleSlider({
+	$('#slider10 .slider').prrpleSlider({
 		direction:				'vertical',
 		transition: 			'slide',
 		loop:					true,
@@ -91,14 +91,14 @@ $(document).ready(function(){
 	
 	
 	/********** SLIDER 12 - SLIDE - VERTICAL - LOOP - SEAMLESS - RICH SWIPING - CSS TRANSFORMS **********/
-	/*$('#slider12 .slider').prrpleSlider({
+	$('#slider12 .slider').prrpleSlider({
 		direction:				'vertical',
 		transition: 			'slide',
 		loop:					true,
 		loopSeamless:			true,
 		richSwiping:			true,
 		csstransforms:			true
-	});*/
+	});
 	
 	
 });
