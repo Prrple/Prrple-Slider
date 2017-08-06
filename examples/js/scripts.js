69fdd66e1995c8ce28957ba2cd23e641d23d6015
Prrple <info@prrple.com>
2017-08-06 12:12:26 +0100

    Website design updates

--- examples/js/scripts.js
+++ examples/js/scripts.js
@@ -4,95 +4,104 @@
 $(document).ready(function(){
 	
 	
-	/********** SLIDER F1 - FADE **********/
+	/********** DEFAULT **********/
+	$('#slider0 .slider').prrpleSlider();
+	
+	
+	/********** EXAMPLE #1 - F1 - FADE **********/
 	$('#sliderf1 .slider').prrpleSlider({
 		transition: 			'fade',
 		loop:					false
 	});
 	
 	
-	/********** SLIDER F2 - FADE - LOOP **********/
+	/********** EXAMPLE #2 - F2 - FADE + LOOP **********/
 	$('#sliderf2 .slider').prrpleSlider({
 		transition: 			'fade'
 	});
 	
 	
-	/********** SLIDER H1 - SLIDE - HORIZONTAL **********/
+	/********** EXAMPLE #3 - H1 - HORIZONTAL SLIDE **********/
 	$('#sliderh1 .slider').prrpleSlider({
 		loop:					false,
-		loopSeamless:			false,
+		csstransforms:			false,
 		richSwiping:			false
 	});
 	
 	
-	/********** SLIDER H2 - SLIDE - HORIZONTAL - LOOP **********/
+	/********** EXAMPLE #4 - H2 - HORIZONTAL SLIDE + LOOP **********/
 	$('#sliderh2 .slider').prrpleSlider({
 		loopSeamless:			false,
+		csstransforms:			false,
 		richSwiping:			false
 	});
 	
 	
-	/********** SLIDER H3 - SLIDE - HORIZONTAL - LOOP - SEAMLESS **********/
+	/********** EXAMPLE #5 - H3 - HORIZONTAL SLIDE + LOOP + SEAMLESS **********/
 	$('#sliderh3 .slider').prrpleSlider({
+		csstransforms:			false,
 		richSwiping:			false
 	});
 	
 	
-	/********** SLIDER H4 - SLIDE - HORIZONTAL - LOOP - SEAMLESS - RICH SWIPING **********/
-	$('#sliderh4 .slider').prrpleSlider();
-	
-	
-	/********** SLIDER H5 - SLIDE - HORIZONTAL - LOOP - SEAMLESS - RICH SWIPING - CSS TRANSFORMS **********/
-	$('#sliderh5 .slider').prrpleSlider({
-		csstransforms:			true
+	/********** EXAMPLE #6 - H4 - HORIZONTAL SLIDE + LOOP + SEAMLESS + RICH SWIPING **********/
+	$('#sliderh4 .slider').prrpleSlider({
+		csstransforms:			false
 	});
 	
 	
-	/********** SLIDER H6 - MULTIPLE **********/
+	/********** EXAMPLE #7 - H5 - HORIZONTAL SLIDE + LOOP + SEAMLESS + RICH SWIPING + CSS TRANSFORMS **********/
+	$('#sliderh5 .slider').prrpleSlider();
+	
+	
+	/********** EXAMPLE #8 - H6 - HORIZONTAL SLIDE + MULTIPLE SLDIES **********/
 	$('#sliderh6 .slider').prrpleSlider({
 		multiple:4,
 		transitionTime:250
 	});
 	
 	
-	/********** SLIDER V1 - SLIDE - VERTICAL **********/
+	/********** EXAMPLE #9 - V1 - VERTICAL SLIDE **********/
 	$('#sliderv1 .slider').prrpleSlider({
 		direction:				'vertical',
 		loop:					false,
 		loopSeamless:			false,
+		csstransforms:			false,
 		richSwiping:			false
 	});
 	
 	
-	/********** SLIDER V2 - SLIDE - VERTICAL - LOOP **********/
+	/********** EXAMPLE #10 - V2 - VERTICAL SLIDE + LOOP **********/
 	$('#sliderv2 .slider').prrpleSlider({
 		direction:				'vertical',
 		loopSeamless:			false,
+		csstransforms:			false,
 		richSwiping:			false
 	});
 	
 	
-	/********** SLIDER V3 - SLIDE - VERTICAL - LOOP - SEAMLESS **********/
+	/********** EXAMPLE #11 - V3 - VERTICAL SLIDE + LOOP + SEAMLESS **********/
 	$('#sliderv3 .slider').prrpleSlider({
 		direction:				'vertical',
+		csstransforms:			false,
 		richSwiping:			false
 	});
 	
 	
-	/********** SLIDER V4 - SLIDE - VERTICAL - LOOP - SEAMLESS - RICH SWIPING **********/
+	/********** EXAMPLE #12 - V4 - VERTICAL SLIDE + LOOP + SEAMLESS + RICH SWIPING **********/
 	$('#sliderv4 .slider').prrpleSlider({
+		direction:				'vertical',
+		csstransforms:			false
+	});
+	
+	
+	/********** EXAMPLE #13 - V5 - VERTICAL SLIDE + LOOP + SEAMLESS + RICH SWIPING + CSS TRANSFORMS **********/
+	$('#sliderv5 .slider').prrpleSlider({
 		direction:				'vertical'
 	});
 	
 	
-	/********** SLIDER V5 - SLIDE - VERTICAL - LOOP - SEAMLESS - RICH SWIPING - CSS TRANSFORMS **********/
-	$('#sliderv5 .slider').prrpleSlider({
-		direction:				'vertical',
-		csstransforms:			true
-	});
-	
-	
-	/********** SLIDER V6 - MULTIPLE **********/
+	/********** EXAMPLE #14 - V6 - VERTICAL SLIDE + MULTIPLE **********/
 	$('#sliderv6 .slider').prrpleSlider({
 		direction:				'vertical',
 		multiple:3,
